4a5b945fc0accd0413caaf54ecc84996bdac7500
Jeffrey Ha <jha@ucsd.edu>
2021-02-20 15:58:13 -0800

    reformatted code

--- server/index.js
+++ server/index.js
@@ -1,14 +1,10 @@
 const http = require('http');
-const cors = require('cors');
-
 const server = http.createServer();
 const io = require('socket.io')(server, {
     cors: {
         origin: '*'
     }
 });
-
-const SERVER_PORT = (process.env.PORT || 3000);
 
 io.on('connection', client => {
     let id = client.id;
@@ -25,4 +21,5 @@ io.on('connection', client => {
     console.log(`Client [${id}] has connected.`);
 });
 
+const SERVER_PORT = (process.env.PORT || 3000);
 server.listen(SERVER_PORT, () => console.log(`Server started on port ${SERVER_PORT}.`));
