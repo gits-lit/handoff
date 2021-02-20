3ab08f5161563469a0a7d68751c14d002f985c60
chanhenry54 <hechan@ucsd.edu>
2021-02-20 13:29:28 -0800

    added button component

--- client/src/components/Topbar/index.jsx
+++ client/src/components/Topbar/index.jsx
@@ -6,6 +6,7 @@ import './style.scss';
 // All available components
 import { Container } from '../subcomponents/Container';
 import { Text } from '../subcomponents/Text';
+import { Button } from '../subcomponents/Button';
 
 export const Topbar = () => {
   const { connectors } = useEditor();
@@ -17,6 +18,12 @@ export const Topbar = () => {
         variant="contained"
       >
         Text
+      </button>
+      <button
+        ref={(ref) => connectors.create(ref, <Button text="Click me" size="small" />)}
+        variant="contained"
+      >
+        Button
       </button>
       <button
         ref={(ref) => connectors.create(ref, <Element canvas is={Container} padding={20} />)}
