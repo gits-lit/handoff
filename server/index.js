ee87fa05657cc8fdb1c579b19e53ff4710941548
Jeffrey Ha <jha@ucsd.edu>
2021-02-20 15:50:19 -0800

    added cors

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 const http = require('http');
+const cors = require('cors');
 
 const server = http.createServer();
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: '*'
+    }
+});
 
 const SERVER_PORT = (process.env.PORT || 3000);
 
@@ -16,6 +21,8 @@ io.on('connection', client => {
     client.on('edit', data => client.broadcast.emit('edit', data));
     client.on('disconnect', () => client.broadcast.emit('disconnect', id));
     client.broadcast.emit('connect', id);
+
+    console.log(`Client [${id}] has connected.`);
 });
 
 server.listen(SERVER_PORT, () => console.log(`Server started on port ${SERVER_PORT}.`));
