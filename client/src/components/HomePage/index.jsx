a0d35d80fd497e8ca6a2acc21220a7af829fbcaa
Emily Nguyen <emily@computer.org>
2021-02-20 17:09:39 -0800

    Sync mouse cursors

--- client/src/components/HomePage/index.jsx
+++ client/src/components/HomePage/index.jsx
@@ -1,9 +1,10 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { Editor, Frame, Element } from '@craftjs/core';
 import socketIOClient from "socket.io-client";
 
 import {Topbar} from '../Topbar';
 import { StateSaver } from '../StateSaver';
+import Cursor from '../Cursor';
 
 import { Container } from '../subcomponents/Container';
 import { Text } from '../subcomponents/Text';
@@ -13,33 +14,45 @@ const ENDPOINT = "http://127.0.0.1:3000";
 let prevMouseX, prevMouseY, x, y;
 
 const HomePage = () => {
+  const [cursors, setCursors] = useState({}); 
+
   useEffect(() => {
     // Update mouse move
     document.addEventListener('mousemove', e => {
       x = e.offsetX;
       y = e.offsetY;
     });
+    document.addEventListener('drag', e => {
+      console.log('test');
+      x = e.pageX;
+      y = e.pageY;
+    });
+    document.addEventListener('mouseleave', e => {
+      x = 5000;
+      y = 0;
+    });
 
     // Send mouse movement
     const socket = socketIOClient(ENDPOINT);
     const intervalID = window.setInterval(function(){
       if (prevMouseX !== x || prevMouseY !== y) {
-        console.log('emitting move');
-        console.log(x);
-        console.log(y);
         prevMouseX = x;
         prevMouseY = y;
         socket.emit('mousemove', {mx : x, my : y});
       }
-   }, 500);
+   }, 20);
 
     // Handle another mouse movement
     socket.on("mousemove", data => {
-      console.log('got it')
+      const newCursors = {...cursors};
+      newCursors[data.id] = {
+        mx: data.mx,
+        my: data.my,
+        number: Object.keys(cursors).length + 1
+      }
+      setCursors(newCursors);
     });
   }, []);
-
-
 
   return (
     <div>
@@ -63,6 +76,13 @@ const HomePage = () => {
         <Topbar />
         <StateSaver />
       </Editor>
+      {
+        Object.values(cursors).map((cursor) => {
+          return (
+            <Cursor x={cursor.mx} y={cursor.my} number={cursor.number % 3}/>
+          )
+        }) 
+      }
     </div>
   );
 }
