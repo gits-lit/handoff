cb3b826d0799eb11fdbf4c6c1e967f4ef515ea9c
leestanley <stanleylee91776@gmail.com>
2021-02-20 18:28:03 -0800

    owo

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import './index.scss';
 
 import HomePage from './components/HomePage';
 
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+
+      </div>
+    </Router>
+  );
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <HomePage />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
