ba519d58e437333072995f410eea45af0f129473
Jeffrey Ha <jha@ucsd.edu>
2021-02-20 16:50:07 -0800

    added utilities

---
+++ server/util/system.js
@@ -0,0 +1,19 @@
+const db = require('./firebase');
+const api = {
+    createError: (message, data) => {
+        return {
+            success: false,
+            message,
+            ...data
+        };
+    },
+    createSuccess: (data) => {
+        return {
+            success: true,
+            ...data
+        };
+    },
+    
+};
+
+module.exports = api;
