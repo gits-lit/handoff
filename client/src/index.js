27aef77f11450d476fc8a25dd02e944c7ca700ed
leestanley <stanleylee91776@gmail.com>
2021-02-20 21:25:47 -0800

    landing

--- client/src/index.js
+++ client/src/index.js
@@ -5,18 +5,20 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './index.scss';
 
 import HomePage from './components/HomePage';
+import LandingPage from './components/LandingPage'
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Switch>
-
-          <Route path="/">
+          <Route path="/app">
             <HomePage />
           </Route>
+          <Route path="/">
+            <LandingPage />
+          </Route>
         </Switch>
-
       </div>
     </Router>
   );
